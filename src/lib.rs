ea07d6eafda0ee5256ecd28885af2e9658fc354a
Robert Jang <robbaldwin95@gmail.com>
2024-05-11 22:27:40 -0700

    export BufferOwner

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,9 @@ use log::{debug, trace};
 mod buffer;
 #[cfg(any(test, feature = "test"))]
 pub use buffer::{binary_data, byte_str};
-pub use buffer::{fill, BinaryData, ByteStr, Owned, OwnedImpl, Pool, PoolImpl, Shared, SharedImpl};
+pub use buffer::{
+    fill, BinaryData, BufferOwner, ByteStr, Owned, OwnedImpl, Pool, PoolImpl, Shared, SharedImpl,
+};
 
 mod codes;
 pub use codes::{
