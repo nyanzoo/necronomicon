3171c2b3035716cc1a98c45d13add9d594f92d39
Robert Jang <robbaldwin95@gmail.com>
2024-05-18 22:24:42 -0700

    pool acquire infallible

--- src/buffer/pool.rs
+++ src/buffer/pool.rs
@@ -136,15 +136,15 @@ mod tests {
     #[test]
     fn acquire() {
         let pool = PoolImpl::new(1024, 1);
-        let buffer = pool.acquire("test").unwrap();
+        let buffer = pool.acquire("test");
         assert_eq!(buffer.unfilled_capacity(), 1024);
     }
 
     #[test]
     fn acquire_multiple() {
         let pool = PoolImpl::new(1024, 2);
-        let buffer1 = pool.acquire("test").unwrap();
-        let buffer2 = pool.acquire("test").unwrap();
+        let buffer1 = pool.acquire("test");
+        let buffer2 = pool.acquire("test");
         assert_eq!(buffer1.unfilled_capacity(), 1024);
         assert_eq!(buffer2.unfilled_capacity(), 1024);
     }
@@ -152,24 +152,24 @@ mod tests {
     #[test]
     fn acquire_release() {
         let pool = PoolImpl::new(1024, 1);
-        let buffer = pool.acquire("test").unwrap();
+        let buffer = pool.acquire("test");
         assert_eq!(buffer.unfilled_capacity(), 1024);
         drop(buffer);
-        let buffer = pool.acquire("test").unwrap();
+        let buffer = pool.acquire("test");
         assert_eq!(buffer.unfilled_capacity(), 1024);
     }
 
     #[test]
     fn acquire_release_multiple() {
         let pool = PoolImpl::new(1024, 2);
-        let buffer1 = pool.acquire("test").unwrap();
-        let buffer2 = pool.acquire("test").unwrap();
+        let buffer1 = pool.acquire("test");
+        let buffer2 = pool.acquire("test");
         assert_eq!(buffer1.unfilled_capacity(), 1024);
         assert_eq!(buffer2.unfilled_capacity(), 1024);
         drop(buffer1);
         drop(buffer2);
-        let mut buffer1 = pool.acquire("test").unwrap();
-        let buffer2 = pool.acquire("test").unwrap();
+        let mut buffer1 = pool.acquire("test");
+        let buffer2 = pool.acquire("test");
         assert_eq!(buffer1.unfilled_capacity(), 1024);
         assert_eq!(buffer2.unfilled_capacity(), 1024);
         let buffer3 = buffer1.split_at(512);
@@ -178,7 +178,18 @@ mod tests {
         let buffer1 = buffer1.into_shared();
         drop(buffer1);
         drop(buffer3);
-        let buffer1 = pool.acquire("test").unwrap();
+        let buffer1 = pool.acquire("test");
         assert_eq!(buffer1.unfilled_capacity(), 1024);
+    }
+
+    #[cfg(feature = "timeout")]
+    #[test]
+    #[should_panic]
+    fn pool_clone_keeps_capacity() {
+        let pool = PoolImpl::new(1024, 1);
+        let buffer = pool.acquire("test");
+        assert_eq!(buffer.unfilled_capacity(), 1024);
+        let pool = pool.clone();
+        let _buffer = pool.acquire("test");
     }
 }
