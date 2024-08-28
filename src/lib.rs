556ede08211ebdcf2823b7fce8025d1825c70349
Robert T Jang <robbaldwin95@gmail.com>
2024-08-28 11:05:58 -0700

    log the header in decode

--- src/lib.rs
+++ src/lib.rs
@@ -369,6 +369,7 @@ where
         Header::decode(reader)?
     };
 
+    trace!("header '{:?}'", header);
     if header.len > buffer.unfilled_capacity() {
         return Err(Error::BufferTooSmallForPacketDecode {
             header,
